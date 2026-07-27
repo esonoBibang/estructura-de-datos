876f7e8c8f1362ea6bfaa1b8970e1601892a2db2
esonoBibang <antonioesonobibang@g,ail.com>
2026-07-27 21:26:54 +0800

    Invertir_texto

---
+++ invertirTexto_stack.py
@@ -0,0 +1,34 @@
+#Problema: Crea una función que reciba una palabra o frase y devuelva el texto invertido usando una pila.
+
+#Idea: Al meter cada letra en la pila y luego sacarlas una a una, saldrán en orden inverso.
+
+
+def invertir_texto(word):
+
+    count = 1
+    invertido =[]
+    stack = []
+    palabra = ""
+
+    if len(stack) == 0:
+        for l in word:
+            stack.append(l) #poner en la pila
+   
+    while len(stack) >= count:
+        
+        invertido.append(stack[-count])
+        count += 1
+
+    for l in invertido:
+        palabra = palabra + stack.pop() # vaciamos el stack en orden invertido
+
+
+    return f'la palabra invertida {palabra} y el stack ---> {stack}'
+
+print(invertir_texto("Antonio esono"))
+
+
+        
+
+
+    
